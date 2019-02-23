e1efc1fc4adbcc5d53c8c3d5414d4161d02092a7
thajedster <jedlawrencecheng@gmail.com>
2019-02-23 12:21:38 -0500

    fixed scope

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,39 +1,39 @@
-var alphabet = 'abcdefghijklmnopqrstuvwxyz';
-var letterList =  alphabet.split('');
-
-var userguess = document.getElementById("userGuesses");
-var wins = document.getElementById("wins");
-var losses = document.getElementById("losses");
-var guessLeft = document.getElementById("numGuesses");
+var alphabet = "abcdefghijklmnopqrstuvwxyz";
+var letterList = alphabet.split("");
 
 var winCounter = 0;
 var lossCounter = 0;
 var guessCounter = 9;
 var guessArray = [];
 
-document.onkeyup = function(event){
-    var appLetter = letterList[Math.floor(Math.random() * letterList.length)];
+document.onkeyup = function(event) {
+  var appLetter = letterList[Math.floor(Math.random() * letterList.length)];
+  var userguess = document.getElementById("userGuesses");
+  var wins = document.getElementById("wins");
+  var losses = document.getElementById("losses");
+  var guessLeft = document.getElementById("numGuesses");
 
-    if(event.key === appLetter){
-        winCounter++;
-        wins.textContent = winCounter;
-        guessCounter = 9;
-        guessArray = [];
-        guessLeft.textContent = 9;
-        userguess.textContent = " ";
-    }else if(event.key !== appLetter){
-        guessArray.push(event.key);
-        userguess.textContent = guessArray;
-        guessCounter = guessCounter - 1;
-        guessLeft.textContent = guessCounter;
-        //guessLeft.textContent = ;
-        if (guessCounter === 0){
-            guessCounter = 9;
-            guessArray = [];
-            guessLeft.textContent = 9;
-            userguess.textContent = " ";
-            lossCounter++;
-            losses.textContent = lossCounter;
-        }
+  if (event.key === appLetter) {
+    winCounter++;
+    wins.textContent = winCounter;
+    guessCounter = 9;
+    guessArray = [];
+    userguess.textContent = guessArray;
+    guessLeft.textContent = 9;
+    userguess.textContent = " ";
+  } else if (event.key !== appLetter) {
+    guessArray.push(event.key);
+    userguess.textContent = guessArray;
+    guessCounter = guessCounter - 1;
+    guessLeft.textContent = guessCounter;
+    //guessLeft.textContent = ;
+    if (guessCounter === 0) {
+      guessCounter = 9;
+      guessArray = [];
+      guessLeft.textContent = 9;
+      userguess.textContent = " ";
+      lossCounter++;
+      losses.textContent = lossCounter;
     }
-}
+  }
+};
